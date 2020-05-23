93fb9a4f550b1b77807d13043c0277650ab03608
crystalDf <chendong333@gmail.com>
2020-05-23 16:39:47 +0800

    Chapter 05 FragmentTest

--- app/src/main/java/com/star/fragmenttest/MainActivity.kt
+++ app/src/main/java/com/star/fragmenttest/MainActivity.kt
@@ -2,11 +2,26 @@ package com.star.fragmenttest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_left.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        button.setOnClickListener { replaceFragment(AnotherRightFragment()) }
+//
+//        replaceFragment(RightFragment())
     }
+
+//    private fun replaceFragment(fragment: Fragment) {
+//
+//        val fragmentManager = supportFragmentManager
+//        val fragmentTransaction = fragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.rightLayout, fragment)
+//        fragmentTransaction.addToBackStack(null)
+//        fragmentTransaction.commit()
+//    }
 }
